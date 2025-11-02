027ee5e3d882ceb39d5f3298b077cca8f475bbda
sb-dor <avaz.shams.2002@gmail.com>
2025-11-02 18:34:17 +0500

    test -2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,12 +4,10 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_docker/main.dart';
-
 void main() {
-  expect(actual, matcher)
+  test('Sample test', () {
+    expect(2 + 2, equals(4));
+  });
 }
